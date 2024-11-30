7e0dd86726f0bb2346c822a393a197c66874f14d
Galleta <galletadecanela2048@gmail.com>
2024-11-29 18:12:05 -0600

    Laboratorio 3

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿
+namespace lab3S_VH23017
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSiguienteCliente = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.txtHoras = new System.Windows.Forms.MaskedTextBox();
+            this.txtTelefono = new System.Windows.Forms.MaskedTextBox();
+            this.txtIdioma = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCantClientes = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMenorVenta = new System.Windows.Forms.TextBox();
+            this.dgvRegistros = new System.Windows.Forms.DataGridView();
+            this.nombreCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idioma = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.horas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Instituto Espanglish";
+            // 
+            // btnSiguienteCliente
+            // 
+            this.btnSiguienteCliente.Location = new System.Drawing.Point(123, 487);
+            this.btnSiguienteCliente.Name = "btnSiguienteCliente";
+            this.btnSiguienteCliente.Size = new System.Drawing.Size(114, 23);
+            this.btnSiguienteCliente.TabIndex = 1;
+            this.btnSiguienteCliente.Text = "Siguiente Cliente";
+            this.btnSiguienteCliente.UseVisualStyleBackColor = true;
+            this.btnSiguienteCliente.Click += new System.EventHandler(this.btnSiguienteCliente_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(313, 487);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 3;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // txtHoras
+            // 
+            this.txtHoras.Location = new System.Drawing.Point(245, 252);
+            this.txtHoras.Mask = "99";
+            this.txtHoras.Name = "txtHoras";
+            this.txtHoras.Size = new System.Drawing.Size(100, 20);
+            this.txtHoras.TabIndex = 6;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(84, 252);
+            this.txtTelefono.Mask = "99999999";
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefono.TabIndex = 6;
+            // 
+            // txtIdioma
+            // 
+            this.txtIdioma.Location = new System.Drawing.Point(206, 68);
+            this.txtIdioma.Name = "txtIdioma";
+            this.txtIdioma.Size = new System.Drawing.Size(100, 20);
+            this.txtIdioma.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(46, 68);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Cantidad de clientes";
+            // 
+            // txtCantClientes
+            // 
+            this.txtCantClientes.Location = new System.Drawing.Point(236, 78);
+            this.txtCantClientes.Mask = "99";
+            this.txtCantClientes.Name = "txtCantClientes";
+            this.txtCantClientes.Size = new System.Drawing.Size(100, 20);
+            this.txtCantClientes.TabIndex = 6;
+            this.txtCantClientes.ValidatingType = typeof(int);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nombre del cliente";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(206, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Idioma";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Telefono";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(206, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Horas";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtIdioma);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(39, 123);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 165);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cliente";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(384, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Menor venta";
+            // 
+            // txtMenorVenta
+            // 
+            this.txtMenorVenta.Location = new System.Drawing.Point(384, 191);
+            this.txtMenorVenta.Name = "txtMenorVenta";
+            this.txtMenorVenta.Size = new System.Drawing.Size(100, 20);
+            this.txtMenorVenta.TabIndex = 5;
+            // 
+            // dgvRegistros
+            // 
+            this.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombreCliente,
+            this.telefono,
+            this.idioma,
+            this.horas,
+            this.precio});
+            this.dgvRegistros.Location = new System.Drawing.Point(24, 319);
+            this.dgvRegistros.Name = "dgvRegistros";
+            this.dgvRegistros.ReadOnly = true;
+            this.dgvRegistros.Size = new System.Drawing.Size(544, 150);
+            this.dgvRegistros.TabIndex = 8;
+            // 
+            // nombreCliente
+            // 
+            this.nombreCliente.HeaderText = "Nombre";
+            this.nombreCliente.Name = "nombreCliente";
+            this.nombreCliente.ReadOnly = true;
+            // 
+            // telefono
+            // 
+            this.telefono.HeaderText = "Telefono";
+            this.telefono.Name = "telefono";
+            this.telefono.ReadOnly = true;
+            // 
+            // idioma
+            // 
+            this.idioma.HeaderText = "Idioma";
+            this.idioma.Name = "idioma";
+            this.idioma.ReadOnly = true;
+            // 
+            // horas
+            // 
+            this.horas.HeaderText = "Horas";
+            this.horas.Name = "horas";
+            this.horas.ReadOnly = true;
+            // 
+            // precio
+            // 
+            this.precio.HeaderText = "Precio";
+            this.precio.Name = "precio";
+            this.precio.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 535);
+            this.Controls.Add(this.dgvRegistros);
+            this.Controls.Add(this.txtCantClientes);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtMenorVenta);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtHoras);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnSiguienteCliente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSiguienteCliente;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.MaskedTextBox txtHoras;
+        private System.Windows.Forms.MaskedTextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtIdioma;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox txtCantClientes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMenorVenta;
+        private System.Windows.Forms.DataGridView dgvRegistros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idioma;
+        private System.Windows.Forms.DataGridViewTextBoxColumn horas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precio;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3S_VH23017
+{
+    public partial class Form1 : Form
+    {
+        int limite = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSiguienteCliente_Click(object sender, EventArgs e)
+        {
+            string[] nombre = new string[limite];
+            string[] idioma = new string[limite];
+            int[] telefono = new int[limite];
+            int[] horas = new int[limite];
+            int precioHora;
+
+            limite = Convert.ToInt16(txtCantClientes.Text);
+
+            nombre[limite] = txtNombre.Text;
+            idioma[limite] = txtIdioma.Text;
+            telefono[limite] = Convert.ToInt32(txtTelefono.Text);
+            horas[limite] = Convert.ToInt32(txtHoras.Text);
+
+            precioHora = horas[limite]* 25;
+
+            dgvRegistros.Rows.Add(nombre[limite], telefono[limite], idioma[limite], horas[limite], precioHora);
+        }
+    }
+}
